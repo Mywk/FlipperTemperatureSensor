f20150d672af4bdd638ea23a6654f71bf0366d71
Mywk <mywk@msn.com>
2022-10-09 22:31:08 +0200

    ~ Fixed formatting
    ~ Removed unnecessary check
    + A little bit more documentation

--- temperature_sensor/temperature_sensor.c
+++ temperature_sensor/temperature_sensor.c
@@ -24,23 +24,22 @@
 #define I2C_BUS &furi_hal_i2c_handle_external
 
 typedef enum {
-    TSSInitializing,
-    TSSNoSensor,
-    TSSPendingUpdate,
+	TSSInitializing,
+	TSSNoSensor,
+	TSSPendingUpdate,
 } TSStatus;
 
 typedef enum {
-    TSEventTypeTick,
-    TSEventTypeInput,
+	TSEventTypeTick,
+	TSEventTypeInput,
 } TSEventType;
 
 typedef struct {
-    TSEventType type;
-    InputEvent input;
+	TSEventType type;
+	InputEvent input;
 } TSEvent;
 
 extern const NotificationSequence sequence_blink_red_100;
-extern const NotificationSequence sequence_blink_green_100;
 extern const NotificationSequence sequence_blink_blue_100;
 
 static TSStatus temperature_sensor_current_status = TSSInitializing;
@@ -57,33 +56,36 @@ char ts_data_buffer_humidity[DATA_BUFFER_SIZE];
 // true if fetch was successful, false otherwise
 // </returns>
 static bool temperature_sensor_cmd(uint8_t cmd, uint8_t* buffer, uint8_t size) {
-	
-    uint32_t timeout = furi_ms_to_ticks(100);
-    bool ret = false;
 
-    furi_hal_i2c_acquire(I2C_BUS);
-    if(furi_hal_i2c_is_device_ready(I2C_BUS, HTU21D_ADDRESS, timeout))
+	uint32_t timeout = furi_ms_to_ticks(100);
+	bool ret = false;
+
+	// Aquire I2C and check if device is ready, then release
+	furi_hal_i2c_acquire(I2C_BUS);
+	if (furi_hal_i2c_is_device_ready(I2C_BUS, HTU21D_ADDRESS, timeout))
 	{
-        furi_hal_i2c_release(I2C_BUS);
+		furi_hal_i2c_release(I2C_BUS);
 
-        furi_hal_i2c_acquire(I2C_BUS);
-        ret = furi_hal_i2c_tx(I2C_BUS, HTU21D_ADDRESS, &cmd, 1, timeout);
-        furi_hal_i2c_release(I2C_BUS);
+		furi_hal_i2c_acquire(I2C_BUS);
+		// Transmit given command
+		ret = furi_hal_i2c_tx(I2C_BUS, HTU21D_ADDRESS, &cmd, 1, timeout);
+		furi_hal_i2c_release(I2C_BUS);
 
-        if(ret)
+		if (ret)
 		{
-            uint32_t wait_ticks = furi_ms_to_ticks(50);
-            furi_delay_tick(wait_ticks);
+			uint32_t wait_ticks = furi_ms_to_ticks(50);
+			furi_delay_tick(wait_ticks);
 
-            furi_hal_i2c_acquire(I2C_BUS);
-            ret = furi_hal_i2c_rx(I2C_BUS, HTU21D_ADDRESS, buffer, size, timeout);
-            furi_hal_i2c_release(I2C_BUS);
-        }
-    }
+			furi_hal_i2c_acquire(I2C_BUS);
+			// Receive data
+			ret = furi_hal_i2c_rx(I2C_BUS, HTU21D_ADDRESS, buffer, size, timeout);
+			furi_hal_i2c_release(I2C_BUS);
+		}
+	}
 	else
-        furi_hal_i2c_release(I2C_BUS);
+		furi_hal_i2c_release(I2C_BUS);
 
-    return ret;
+	return ret;
 }
 
 // <sumary>
@@ -91,39 +93,39 @@ static bool temperature_sensor_cmd(uint8_t cmd, uint8_t* buffer, uint8_t size) {
 // </sumary>
 // <remarks>
 // Temperature and humidity must be preallocated
-// TODO: CRC
+// Note: CRC is not checked (3rd byte)
 // </remarks>
 // <returns>
 // true if fetch was successful, false otherwise
 // </returns>
 static bool temperature_sensor_fetch_data(double* temperature, double* humidity) {
-    bool ret = false;
+	bool ret = false;
 
-    uint16_t adc_raw;
+	uint16_t adc_raw;
 
-    uint8_t buffer[2] = { 0x00 };
+	uint8_t buffer[2] = { 0x00 };
 
-    // Fetch temperature
-    ret = temperature_sensor_cmd((uint8_t)HTU21D_CMD_TEMPERATURE, buffer, 2);
+	// Fetch temperature
+	ret = temperature_sensor_cmd((uint8_t)HTU21D_CMD_TEMPERATURE, buffer, 2);
 
-    if(ret)
+	if (ret)
 	{
 		// Calculate temperature
-        adc_raw = ((uint16_t)(buffer[0] << 8) | (buffer[1]));
-        *temperature = (float)(adc_raw * 175.72 / 65536.00) - 46.85;
+		adc_raw = ((uint16_t)(buffer[0] << 8) | (buffer[1]));
+		*temperature = (float)(adc_raw * 175.72 / 65536.00) - 46.85;
 
-        // Fetch humidity
-        ret = temperature_sensor_cmd((uint8_t)HTU21D_CMD_TEMPERATURE, buffer, 2);
+		// Fetch humidity
+		ret = temperature_sensor_cmd((uint8_t)HTU21D_CMD_TEMPERATURE, buffer, 2);
 
-        if(ret)
+		if (ret)
 		{
-            // Calculate humidity
-            adc_raw = ((uint16_t)(buffer[0] << 8) | (buffer[1]));
-            *humidity = (float)(adc_raw * 125.0 / 65536.0) - 6.0;
-        }
+			// Calculate humidity
+			adc_raw = ((uint16_t)(buffer[0] << 8) | (buffer[1]));
+			*humidity = (float)(adc_raw * 125.0 / 65536.00) - 6.0;
+		}
 	}
 
-    return ret;
+	return ret;
 }
 
 
@@ -132,46 +134,46 @@ static bool temperature_sensor_fetch_data(double* temperature, double* humidity)
 // </sumary>
 static void temperature_sensor_draw_callback(Canvas* canvas, void* ctx) {
 
-    UNUSED(ctx);
+	UNUSED(ctx);
 
 	canvas_clear(canvas);
 	canvas_set_font(canvas, FontPrimary);
 	canvas_draw_str(canvas, 2, 10, "HTU21D Sensor");
 
-    canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str(canvas, 2, 62, "Press back to exit.");
+	canvas_set_font(canvas, FontSecondary);
+	canvas_draw_str(canvas, 2, 62, "Press back to exit.");
 
-    switch(temperature_sensor_current_status) {
-    case TSSInitializing:
-        canvas_draw_str(canvas, 2, 30, "Initializing..");
-        break;
-    case TSSNoSensor:
-        canvas_draw_str(canvas, 2, 30, "No sensor found!");
-        break;
-    case TSSPendingUpdate: {
+	switch (temperature_sensor_current_status) {
+	case TSSInitializing:
+		canvas_draw_str(canvas, 2, 30, "Initializing..");
+		break;
+	case TSSNoSensor:
+		canvas_draw_str(canvas, 2, 30, "No sensor found!");
+		break;
+	case TSSPendingUpdate: {
 
-        canvas_draw_str(canvas, 6, 24, "Temperature");
-        canvas_draw_str(canvas, 80, 24, "Humidity");
+		canvas_draw_str(canvas, 6, 24, "Temperature");
+		canvas_draw_str(canvas, 80, 24, "Humidity");
 
-        // Draw vertical lines
-        canvas_draw_line(canvas, 68, 16, 68, 50);
-        canvas_draw_line(canvas, 69, 16, 69, 50);
+		// Draw vertical lines
+		canvas_draw_line(canvas, 68, 16, 68, 50);
+		canvas_draw_line(canvas, 69, 16, 69, 50);
 
-        // Draw horizontal line
-        canvas_draw_line(canvas, 3, 27, 144, 27);
+		// Draw horizontal line
+		canvas_draw_line(canvas, 3, 27, 144, 27);
 
 		// Draw temperature and humidity values
-        canvas_draw_str(canvas, 14, 38, ts_data_buffer_temperature_c);
-        canvas_draw_str(canvas, 48, 38, "C");
-        canvas_draw_str(canvas, 14, 48, ts_data_buffer_temperature_f);
-        canvas_draw_str(canvas, 48, 48, "F");
-        canvas_draw_str(canvas, 78, 42, ts_data_buffer_humidity);
-        canvas_draw_str(canvas, 112, 42, "%");
+		canvas_draw_str(canvas, 14, 38, ts_data_buffer_temperature_c);
+		canvas_draw_str(canvas, 48, 38, "C");
+		canvas_draw_str(canvas, 14, 48, ts_data_buffer_temperature_f);
+		canvas_draw_str(canvas, 48, 48, "F");
+		canvas_draw_str(canvas, 78, 42, ts_data_buffer_humidity);
+		canvas_draw_str(canvas, 112, 42, "%");
 
-    } break;
-    default:
-        break;
-    }
+	} break;
+	default:
+		break;
+	}
 
 }
 
@@ -180,11 +182,11 @@ static void temperature_sensor_draw_callback(Canvas* canvas, void* ctx) {
 // </sumary>
 static void temperature_sensor_input_callback(InputEvent* input_event, void* ctx) {
 
-    furi_assert(ctx);
-    FuriMessageQueue* event_queue = ctx;
+	furi_assert(ctx);
+	FuriMessageQueue* event_queue = ctx;
 
-    TSEvent event = {.type = TSEventTypeInput, .input = *input_event};
-    furi_message_queue_put(event_queue, &event, FuriWaitForever);
+	TSEvent event = { .type = TSEventTypeInput, .input = *input_event };
+	furi_message_queue_put(event_queue, &event, FuriWaitForever);
 
 }
 
@@ -192,10 +194,12 @@ static void temperature_sensor_input_callback(InputEvent* input_event, void* ctx
 // Timer callback
 // </sumary>
 static void temperature_sensor_timer_callback(FuriMessageQueue* event_queue) {
-    furi_assert(event_queue);
 
-    TSEvent event = {.type = TSEventTypeTick};
-    furi_message_queue_put(event_queue, &event, 0);
+	furi_assert(event_queue);
+
+	TSEvent event = { .type = TSEventTypeTick };
+	furi_message_queue_put(event_queue, &event, 0);
+
 }
 
 // <sumary>
@@ -203,77 +207,93 @@ static void temperature_sensor_timer_callback(FuriMessageQueue* event_queue) {
 // </sumary>
 int32_t temperature_sensor_app(void* p) {
 
-    UNUSED(p);
+	UNUSED(p);
 
-    TSEvent event;
-    bool sensorFound = false;
-    double celsius, fahrenheit, humidity;
+	// Declare our variables
+	TSEvent tsEvent;
+	bool sensorFound = false;
+	double celsius, fahrenheit, humidity;
 
-    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(TSEvent));
+	FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(TSEvent));
 
-    // Register callbacks
-    ViewPort* view_port = view_port_alloc();
-    view_port_draw_callback_set(view_port, temperature_sensor_draw_callback, NULL);
-    view_port_input_callback_set(view_port, temperature_sensor_input_callback, event_queue);
+	// Register callbacks
+	ViewPort* view_port = view_port_alloc();
+	view_port_draw_callback_set(view_port, temperature_sensor_draw_callback, NULL);
+	view_port_input_callback_set(view_port, temperature_sensor_input_callback, event_queue);
 
-    FuriTimer* timer = furi_timer_alloc(temperature_sensor_timer_callback, FuriTimerTypePeriodic, event_queue);
-    furi_timer_start(timer, furi_kernel_get_tick_frequency());
-	
-    // Register viewport
-    Gui* gui = furi_record_open(RECORD_GUI);
-    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+	// Create timer and register its callback
+	FuriTimer* timer = furi_timer_alloc(temperature_sensor_timer_callback, FuriTimerTypePeriodic, event_queue);
+	furi_timer_start(timer, furi_kernel_get_tick_frequency());
 
-    NotificationApp* notifications = furi_record_open(RECORD_NOTIFICATION);
+	// Register viewport
+	Gui* gui = furi_record_open(RECORD_GUI);
+	gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
-    celsius = fahrenheit = humidity = TS_DEFAULT_VALUE;
+	// Used to notify the user by blinking red (error) or blue (fetch successful)
+	NotificationApp* notifications = furi_record_open(RECORD_NOTIFICATION);
 
-    while(1) {
+	// Assign variables a default value
+	celsius = fahrenheit = humidity = TS_DEFAULT_VALUE;
 
-        furi_check(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk);
+	while (1) {
 
-        // Handle events
-        if(event.type == TSEventTypeInput) {
+		furi_check(furi_message_queue_get(event_queue, &tsEvent, FuriWaitForever) == FuriStatusOk);
 
-            // Exit on back key
-            if(event.input.key == InputKeyBack) // We dont check for type here (event.input.type == InputTypeShort)
-                break;
-        }
-        else if(event.type == TSEventTypeTick) {
-			
-            // Update sensor data
-            // Fetch data and set the sensor current status accordingly
-            sensorFound = temperature_sensor_fetch_data(&celsius, &humidity);
-            temperature_sensor_current_status = (sensorFound ? TSSPendingUpdate : TSSNoSensor);
+		// Handle events
+		if (tsEvent.type == TSEventTypeInput) {
 
-            if(sensorFound) {
-                notification_message(notifications, &sequence_blink_blue_100);
+			// Exit on back key
+			if (tsEvent.input.key == InputKeyBack) // We dont check for type here, we can check the type of keypress like: (event.input.type == InputTypeShort)
+				break;
 
-                if(humidity > 0 && (celsius != TS_DEFAULT_VALUE && celsius != TS_DEFAULT_VALUE)) {
-                    fahrenheit = (celsius * 9 / 5) + 32;
+		}
+		else if (tsEvent.type == TSEventTypeTick) {
 
-                    // Fill our buffers here, not on the canvas draw callback
-                    snprintf(ts_data_buffer_temperature_c, DATA_BUFFER_SIZE, "%.2f", celsius);
-                    snprintf(ts_data_buffer_temperature_f, DATA_BUFFER_SIZE, "%.2f", fahrenheit);
-                    snprintf(ts_data_buffer_humidity, DATA_BUFFER_SIZE, "%.2f", humidity);
-                }
-            } else {
-                celsius = fahrenheit = humidity = TS_DEFAULT_VALUE;
-                notification_message(notifications, &sequence_blink_red_100);
-            }
-        }
+			// Update sensor data
+			// Fetch data and set the sensor current status accordingly
+			sensorFound = temperature_sensor_fetch_data(&celsius, &humidity);
+			temperature_sensor_current_status = (sensorFound ? TSSPendingUpdate : TSSNoSensor);
 
-        uint32_t wait_ticks = furi_ms_to_ticks(!sensorFound ? 100 : 500);
-        furi_delay_tick(wait_ticks);
-    }
+			if (sensorFound) {
 
-	// Dobby is freee
-    furi_timer_free(timer);
-    gui_remove_view_port(gui, view_port);
-    view_port_free(view_port);
-    furi_message_queue_free(event_queue);
+				// Blink blue
+				notification_message(notifications, &sequence_blink_blue_100);
 
-    furi_record_close(RECORD_NOTIFICATION);
-    furi_record_close(RECORD_GUI);
+				if (celsius != TS_DEFAULT_VALUE && humidity != TS_DEFAULT_VALUE)) {
 
-    return 0;
+					// Convert celsius to fahrenheit
+					fahrenheit = (celsius * 9 / 5) + 32;
+
+					// Fill our buffers here, not on the canvas draw callback
+					snprintf(ts_data_buffer_temperature_c, DATA_BUFFER_SIZE, "%.2f", celsius);
+					snprintf(ts_data_buffer_temperature_f, DATA_BUFFER_SIZE, "%.2f", fahrenheit);
+					snprintf(ts_data_buffer_humidity, DATA_BUFFER_SIZE, "%.2f", humidity);
+				}
+
+			}
+			else {
+
+				// Reset our variables to their default values
+				celsius = fahrenheit = humidity = TS_DEFAULT_VALUE;
+
+				// Blink red
+				notification_message(notifications, &sequence_blink_red_100);
+
+			}
+		}
+
+		uint32_t wait_ticks = furi_ms_to_ticks(!sensorFound ? 100 : 500);
+		furi_delay_tick(wait_ticks);
+	}
+
+	// Dobby is freee (free our variables, Flipper will crash if we don't do this!)
+	furi_timer_free(timer);
+	gui_remove_view_port(gui, view_port);
+	view_port_free(view_port);
+	furi_message_queue_free(event_queue);
+
+	furi_record_close(RECORD_NOTIFICATION);
+	furi_record_close(RECORD_GUI);
+
+	return 0;
 }
